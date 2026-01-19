443fac1fc156db76f664d769a77bc9ff194b6c0b
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-01-19 17:45:51 +0530

    creating variables and using some mathematical operators

---
+++ variables.java
@@ -0,0 +1,30 @@
+Basics (Variables, Input/Output)
+
+//Write a Java program to print “Hello World”.
+  public class Hello{
+    public static void main(String[]args){
+      System.out.println("Hello World");
+    }
+  }
+
+//Write a program to read two numbers and print their sum, difference, product, and quotient
+import java.util.Scanner;
+public class Hello{
+  public static void main(String[]args){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter number value num1 ");
+    int num1=sc.nextInt();
+    System.out.println("enter number value num2 ");
+    int num2=sc.nextInt();
+    int sum = num1+num2;
+    int diff = num1 - num2;
+    int product = num1 * num2;
+    int quotient = num1 %num2;
+    System.out.println("the sum of numbers" + sum);
+    System.out.println("the diff of numbers" + diff);
+    System.out.println("the product of numbers" + product);
+    System.out.println("the quotient of numbers" + quotient);
+    sc.close();
+  }
+}
+
