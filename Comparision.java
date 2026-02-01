4c0a95b3fc47ef42edae00769050de7782adaf71
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-02-01 15:49:12 +0530

    comparing variables

---
+++ Comparision.java
@@ -0,0 +1,10 @@
+public class Comparision{
+  public static void main(String[]args){
+    int x = 10;
+    int y = 9;
+    System.out.println(x>y);
+    System.out.println(x<y);
+    System.out.println(x>=y);
+    System.out.println(x<=y);
+  }
+}
