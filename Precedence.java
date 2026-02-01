035a71d5243acdcb1d109f19544bbec31c916e87
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-02-01 19:45:20 +0530

    order of operations

---
+++ Precedence.java
@@ -0,0 +1,8 @@
+public class Precedence{
+  public static void main(String[]args){
+    int operation1= 10-2*3;
+    int operation2= 10-(2*3);
+    System.out.println(operation1);
+    System.out.println(operation2);
+  }
+}
