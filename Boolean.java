1383847244cd1cbceffd95a4fce20987529cc5ec
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-02-01 15:56:09 +0530

    LOGICAL OPERATORS

---
+++ Boolean.java
@@ -0,0 +1,8 @@
+public class Boolean{
+ public static void main(String[]args){
+     boolean isLoggedIn=true;
+     boolean isAdmin=false;
+     System.out.println(isLoggedIn && isAdmin);
+     System.out.println(isLoggedIn || isAdmin)
+ }
+}
