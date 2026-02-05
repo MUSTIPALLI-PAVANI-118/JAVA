4b0f0c37af7976d88b52b286863b439894d558ed
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-02-05 20:41:36 +0530

    Components_of_java

---
+++ Components_of_java.java
@@ -0,0 +1,25 @@
+JDK – Java Development Kit (Detailed)
+JDK is a complete software development kit used to write, compile, debug, and run Java programs.
+JDK Contains:
+JRE
+javac (compiler)
+java (launcher)
+javadoc
+debugger
+Role of JDK:
+Mandatory for programmers
+Without JDK, Java program cannot be compiled
+6. JRE – Java Runtime Environment (Detailed)
+JRE provides an environment to execute Java bytecode.
+JRE Components:
+JVM
+Core libraries (java.lang, java.util, etc.)
+Important Point:
+JRE cannot create bytecode
+It can only run .class files
+ JVM – Java Virtual Machine (Very Detailed)
+Definition:
+JVM is an engine that executes Java bytecode and provides runtime environment.
+Important:
+JVM is platform dependent
+Bytecode is platform independent
