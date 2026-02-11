27ff2e0489d34c165b040560786058d6689839fe
pavanireddy20070104 <pavanireddypavani019@gmail.com>
2026-02-11 16:35:30 +0530

    add HelloExample

---
+++ HelloExample.java
@@ -0,0 +1,15 @@
+public class HelloExample {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println("A = " + a);
+        System.out.println("B = " + b);
+        System.out.println("Sum = " + sum);
+        if (sum > 20) {
+            System.out.println("Sum is greater than 20");
+        } else {
+            System.out.println("Sum is 20 or less");
+        }
+    }
+}
