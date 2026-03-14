070fea3dc1f355ea18ad0f294a17055738fc85d4
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-03-14 11:04:08 +0530

    ThisKeywordConstructorDemo

---
+++ ThisKeywordConstructorDemo.java
@@ -0,0 +1,27 @@
+class Student {
+
+    String name;
+    int age;
+
+    Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    void display() {
+        System.out.println("Name: " + name + " Age: " + age);
+    }
+}
+
+public class ThisKeywordConstructorDemo {
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student("Rahul", 20);
+        Student s2 = new Student("Anjali", 19);
+
+        s1.display();
+        s2.display();
+
+    }
+}
