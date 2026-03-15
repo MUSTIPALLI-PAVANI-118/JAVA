c63f27e94b0cb723db950fc55b8b3c2ff3d90c2b
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-03-15 11:59:40 +0530

    AnimalTest

---
+++ AnimalTest.java
@@ -0,0 +1,42 @@
+abstract class Animal {
+    abstract void sound();
+}
+
+class Dog extends Animal {
+
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat extends Animal {
+
+    void sound() {
+        System.out.println("Cat meows");
+    }
+}
+
+class Cow extends Animal {
+
+    void sound() {
+        System.out.println("Cow moos");
+    }
+}
+
+public class AnimalTest {
+
+    public static void main(String[] args) {
+
+        Animal a;
+
+        a = new Dog();
+        a.sound();
+
+        a = new Cat();
+        a.sound();
+
+        a = new Cow();
+        a.sound();
+
+    }
+}
