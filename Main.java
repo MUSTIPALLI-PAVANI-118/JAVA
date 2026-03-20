564eed64cd14c856583f8eebd9778a1817f12de7
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-03-20 13:55:45 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,34 +1,18 @@
-import java.util.Scanner;
+interface Animal {
+    void sound();  
+}
 
-class Student {
-    String name;
-    double marks;  
-
-    // METHOD - DISPLAY STUDENT DETAILS
-    void displayDetails() {
-        System.out.println("\n Student Details:");
-        System.out.println("Name  : " + name);
-        System.out.println("Marks : " + marks);
+class Dog implements Animal {
+    public void sound() {
+        System.out.println("Dog barks");
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        // OBJECT CREATION
-        Student s1 = new Student();
-
-        // INPUT FROM USER
-        System.out.print("Enter Student Name: ");
-        s1.name = sc.nextLine();
-
-        System.out.print("Enter Marks: ");
-        s1.marks = sc.nextDouble();
-
-        // OUTPUT:
-        s1.displayDetails();
-
-        sc.close();
+        Animal obj = new Dog(); 
+        obj.sound();            
     }
 }
+
+
