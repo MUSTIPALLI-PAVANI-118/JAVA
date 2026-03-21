67c82c38d019a3a3bd939f7bcab5679504836007
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-03-21 14:58:02 +0530

    Add files via upload

--- MediaPlayerDemo.java
+++ MediaPlayerDemo.java
@@ -1,47 +1,49 @@
 import java.util.Scanner;
-
 interface MediaPlayer {
     void play();
 }
-
 class AudioPlayer implements MediaPlayer {
+    String audioName;
+    void getAudio(String audioName) {
+        this.audioName = audioName;
+    }
     public void play() {
-        System.out.println("Playing audio...");
+        System.out.println("Playing Audio: " + audioName);
     }
 }
-
 class VideoPlayer implements MediaPlayer {
+    String videoName;
+    void getVideo(String videoName) {
+        this.videoName = videoName;
+    }
     public void play() {
-        System.out.println("Playing video...");
+        System.out.println("Playing Video: " + videoName);
     }
 }
 
 public class MediaPlayerDemo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.println("Choose Media Type:");
         System.out.println("1. Audio");
         System.out.println("2. Video");
-        System.out.print("Enter choice: ");
-
         int choice = sc.nextInt();
-
-        MediaPlayer player; 
-
-        switch (choice) {
-            case 1:
-                player = new AudioPlayer();
-                break;
-            case 2:
-                player = new VideoPlayer();
-                break;
-            default:
-                System.out.println("Invalid choice!");
-                sc.close();
-                return;
+        sc.nextLine(); 
+        MediaPlayer mp; 
+        if (choice == 1) {
+            System.out.print("Enter Audio Name: ");
+            String audio = sc.nextLine();
+            AudioPlayer ap = new AudioPlayer();
+            ap.getAudio(audio);
+            mp = ap; 
+        } else {
+            System.out.print("Enter Video Name: ");
+            String video = sc.nextLine();
+            VideoPlayer vp = new VideoPlayer();
+            vp.getVideo(video);
+            mp = vp; 
         }
-        player.play();
+        mp.play();
         sc.close();
     }
 }
