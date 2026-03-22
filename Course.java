9e6563c3ad863ab37b36672baf0f6ef224eeb592
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-03-22 16:00:09 +0530

    Add files via upload

---
+++ Course.java
@@ -0,0 +1,14 @@
+package course;
+
+public class Course {
+    String courseName;
+
+    public void setCourse(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public void displayCourse() {
+        System.out.println("Course: " + courseName);
+    }
+}
+
