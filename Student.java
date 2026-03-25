a85557d5f70d9fc11faed68699afed8829ae87c1
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-03-25 06:07:24 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,14 @@
+package student;
+
+public class Student {
+    String name;
+
+    public void setStudent(String name) {
+        this.name = name;
+    }
+
+    public void displayStudent() {
+        System.out.println("Student Name: " + name);
+    }
+}
+
