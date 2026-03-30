488972d875cfad80eaa06555bca8db04fb6335f7
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-03-30 14:41:07 +0530

    ThreadClassDemo

---
+++ ThreadClassDemo.java
@@ -0,0 +1,16 @@
+// File: ThreadClassDemo.java
+
+class MyThread extends Thread {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Thread running: " + i);
+        }
+    }
+}
+
+public class ThreadClassDemo {
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread();
+        t1.start(); // start thread
+    }
+}
