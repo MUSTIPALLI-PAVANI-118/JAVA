fbab434f324b67e7d9150e024d6ab2096f5949d8
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-04-05 08:55:56 +0530

    Add files via upload

--- ThreadPriorityDemo.java
+++ ThreadPriorityDemo.java
@@ -1,41 +1,36 @@
-// File: ThreadPriorityDemo.java
+import java.util.Scanner;
 
-class MyThread1 extends Thread {
-    public void run() {
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("Low Priority Thread: " + i);
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                System.out.println(e);
-            }
-        }
+class MyThread extends Thread {
+    MyThread(String name) {
+        super(name);
     }
-}
 
-class MyThread2 extends Thread {
     public void run() {
         for (int i = 1; i <= 5; i++) {
-            System.out.println("High Priority Thread: " + i);
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                System.out.println(e);
-            }
+            System.out.println(getName() + " is running");
         }
     }
 }
 
 public class ThreadPriorityDemo {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        MyThread1 t1 = new MyThread1();
-        MyThread2 t2 = new MyThread2();
+        System.out.print("Enter priority for Thread 1 (1 to 10): ");
+        int p1 = sc.nextInt();
 
-        t1.setPriority(Thread.MIN_PRIORITY);
-        t2.setPriority(Thread.MAX_PRIORITY);
+        System.out.print("Enter priority for Thread 2 (1 to 10): ");
+        int p2 = sc.nextInt();
+
+        MyThread t1 = new MyThread("Thread 1");
+        MyThread t2 = new MyThread("Thread 2");
+
+        t1.setPriority(p1);
+        t2.setPriority(p2);
 
         t1.start();
         t2.start();
+
+        sc.close();
     }
 }
