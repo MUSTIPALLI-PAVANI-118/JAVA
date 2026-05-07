ab8c3a5ca562f2ec677d5996e901999b698a0d8f
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-05-07 06:47:22 +0530

    Create ArraySorting.java

---
+++ ArraySorting.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArraySorting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        Arrays.sort(arr);
+
+        System.out.println("Sorted Array:");
+
+        for (int num : arr)
+            System.out.print(num + " ");
+    }
+}
