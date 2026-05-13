c020aff24db7d7af12907a5f53d5238e84da1dfd
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-05-13 15:36:23 +0530

    Create SimpleCalculator.java

---
+++ SimpleCalculator.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double a = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double b = sc.nextDouble();
+
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+
+        System.out.print("Choose operation: ");
+        int choice = sc.nextInt();
+
+        switch (choice) {
+
+            case 1:
+                System.out.println("Result: " + (a + b));
+                break;
+
+            case 2:
+                System.out.println("Result: " + (a - b));
+                break;
+
+            case 3:
+                System.out.println("Result: " + (a * b));
+                break;
+
+            case 4:
+
+                if (b == 0) {
+                    System.out.println("Cannot divide by zero");
+                } else {
+                    System.out.println("Result: " + (a / b));
+                }
+                break;
+
+            default:
+                System.out.println("Invalid Choice");
+        }
+
+        sc.close();
+    }
+}
