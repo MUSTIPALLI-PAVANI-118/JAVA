c619a5ce72598420a2d5396b748202849503d4e4
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-05-13 15:34:32 +0530

    Create PasswordGenerator.java

---
+++ PasswordGenerator.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class PasswordGenerator {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String characters =
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890@#";
+
+        System.out.print("Enter password length: ");
+        int length = sc.nextInt();
+
+        Random random = new Random();
+
+        String password = "";
+
+        for (int i = 0; i < length; i++) {
+
+            int index = random.nextInt(characters.length());
+
+            password += characters.charAt(index);
+        }
+
+        System.out.println("Generated Password: " + password);
+
+        sc.close();
+    }
+}
