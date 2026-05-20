8d83b6704dd3379a98d97b6e59303197e91e6c86
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-05-20 09:18:39 +0530

    Create NumberGuessGame.java

---
+++ NumberGuessGame.java
@@ -0,0 +1,37 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuessGame {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+
+        int secretNumber = random.nextInt(100) + 1;
+
+        int guess;
+
+        System.out.println("===== Number Guess Game =====");
+        System.out.println("Guess a number between 1 and 100");
+
+        do {
+
+            System.out.print("Enter your guess: ");
+            guess = sc.nextInt();
+
+            if (guess > secretNumber) {
+                System.out.println("Too High!");
+            }
+
+            else if (guess < secretNumber) {
+                System.out.println("Too Low!");
+            }
+
+            else {
+                System.out.println("Correct! You guessed the number.");
+            }
+
+        } while (guess != secretNumber);
+    }
+}
