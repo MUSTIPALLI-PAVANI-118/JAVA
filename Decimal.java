bed35e5ad9a7b13f5eac4326c36d1033cee48f70
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-05-22 21:03:47 +0530

    Add files via upload

---
+++ Decimal.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Decimal{
+    public static void main(String[]args){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("enter the value of decimal number:");
+        double num=sc.nextDouble();
+        System.out.println("decimal number:"+num);
+        sc.close();
+    
+    }
+}
