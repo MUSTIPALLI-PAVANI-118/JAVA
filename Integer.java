ff015154ad133659bbcb5d82ced13c20e12607b3
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-05-23 20:19:58 +0530

    Add files via upload

---
+++ Integer.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Integer{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the value of integer:");
+        int num =sc.nextInt();
+        System.out.println("VALUE OF INTEGER IS:"+num);
+        sc.close();
+    }
+}
