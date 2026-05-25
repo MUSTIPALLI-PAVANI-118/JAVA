c25a781493fde1bab791641a06b61fa05220e56d
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-05-25 13:57:40 +0530

    Add files via upload

--- Sum.java
+++ Sum.java
@@ -1,17 +1,12 @@
-//Find **Sum of Digits*
 import java.util.Scanner;
-public class Hello{
-  public static void main(String[]args){
-  Scanner sc = new Scanner(System.in);
-  System.out.println("ENTER THE VALUE OF N:");
-  int n = sc.nextInt();
-  int sum =0;
-  while (n!=0){
-    int digit = n%10;
-    sum = sum  + digit;
-    n = n / 10;
-  }
-  System.out.println("THE SUM OF THE DIGITS OF THE GIVEN NUMBER IS "+ sum);
-    sc.close();
-  }
+public class Sum{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a and b value:");
+        int a =sc.nextInt();
+        int b=sc.nextInt();
+        int sum=a+b;
+        System.out.println("sum of a and b value:"+sum);
+        sc.close();
+    }
 }
