06937e88373cdf3eb7d9542435f4063675cef00f
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-05-27 13:26:25 +0530

    Add files via upload

--- Marks.java
+++ Marks.java
@@ -1,22 +1,23 @@
 import java.util.Scanner;
-public class Hello{
-  public static void main(String[]args){
-    Scanner sc = new Scanner(System.in);
-    System.out.println("ENTER THE MARKS:");
-    int marks=sc.nextInt();
-    if (marks>=90){
-      System.out.println("A grade");
-    }else if(marks>=80){
-      System.out.println("B grade");
-    }else if(marks>=70){
-      System.out.println("C grade");
-    }else if(marks>=60){
-      System.out.println("D grade");
-    }else if(marks>=50){
-      System.out.println("E grade");
-    }else{
-      System.out.println("FAIL");
+public class Marks {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter subject 1 marks:");
+        int a=sc.nextInt();
+        System.out.print("enter subject 2 marks:");
+        int b=sc.nextInt();
+        System.out.print("enter subject 3 marks:");
+        int c=sc.nextInt();
+        System.out.print("enter subject 4 marks:");
+        int d=sc.nextInt();
+        System.out.print("enter subject 5 marks:");
+        int e=sc.nextInt();
+        int total=a+b+c+d+e;
+        double average=(total)/5.0;
+        System.out.println("average of numbers:"+average);
+        double percentage=(total/500.0)*100;
+        System.out.println("percentage of numbers:"+percentage);
+        sc.close();
     }
-    sc.close();
-  }
+    
 }
