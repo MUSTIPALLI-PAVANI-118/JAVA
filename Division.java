a50ce1da3b3b9914580c965dc3d03648c87ae2ba
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-05-31 21:25:56 +0530

    Add files via upload

--- Division.java
+++ Division.java
@@ -2,14 +2,10 @@ import java.util.Scanner;
 public class Division {
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
-        System.out.print("ENTER THE NUMBER:");
-        int num=sc.nextInt();
-        System.out.print("ENTER THE DIVISOR:");
-        int divisor=sc.nextInt();
-        int quotient=num/divisor;
-        int remainder=num%divisor;
-        System.out.println("QUOTIENT IS "+quotient);
-        System.out.println("REMAINDER IS "+remainder);
+        System.out.println("enter the num:");
+        double num=sc.nextDouble();
+        num /= 2.0;
+        System.out.println("finally number is:"+num);
         sc.close();
     }
 }
