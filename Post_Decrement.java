b8eba10fa7427a62b8a9d4c747995c73e0079fa4
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-06-02 00:36:12 +0530

    Add files via upload

---
+++ Post_Decrement.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Post_Decrement {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the num value");
+        int num=sc.nextInt();
+        System.out.println("number before decrement is:"+num);
+        int n=num--;
+        System.out.println("number after decrement is:"+n);
+        sc.close();
+    }
+}
