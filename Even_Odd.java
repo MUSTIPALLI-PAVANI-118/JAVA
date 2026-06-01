161555b2c6412ff53df7eae2e3ec28c4e42a4d29
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-06-01 21:15:00 +0530

    Add files via upload

--- Even_Odd.java
+++ Even_Odd.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
-public class Even_Odd { 
+public class Even_Odd {
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter the number:");
-        int num=sc.nextInt();
-        if(num%2==0){
-            System.out.println("NUMBER IS EVEN");
+        System.out.println("enter the number");
+        int n=sc.nextInt();
+        if(n%2==0){
+            System.out.println("even");
         }
         else{
-            System.out.println("NUMBER IS ODD");
+            System.out.println("Odd");
         }
         sc.close();
     }
+    
 }
