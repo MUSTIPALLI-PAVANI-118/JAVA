5e9b42a08630d6529844cfc7602756385844fa12
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-06-03 20:51:47 +0530

    Add files via upload

---
+++ Subtract.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Subtract {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the num:");
+        int num=sc.nextInt();
+        num -= 1;
+        System.out.println("finally number is:"+num);
+        sc.close();
+    }
+}
