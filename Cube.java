940bbab345664e1a8e9396653461b4e5939f50a6
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-06-07 22:13:10 +0530

    Add files via upload

---
+++ Cube.java
@@ -0,0 +1,7 @@
+public class Cube {
+    public static void main(String[]args){
+        for(int i=1;i<=20;i++){
+            System.out.println("cube of number "+i+" is "+(i*i*i));
+        }
+    }    
+}
