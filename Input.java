2433c8e4c620486395f231ab4d3afaac77b21cf8
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-06-08 15:23:42 +0530

    Add files via upload

--- Input.java
+++ Input.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
-public class Input{
+public class Input {
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter integer value:");
-        int a=sc.nextInt();
-        System.out.println("integer value is:"+a);
-        System.out.println("enter decimal value:");
-        double b=sc.nextDouble();
-        System.out.println("decimal value is:"+b);
+        System.out.println("enter num:");
+        int num;
+        while(true){
+            num=sc.nextInt();
+            if(num==-1){
+                break;
+            }
+        }
         sc.close();
     }
 }
