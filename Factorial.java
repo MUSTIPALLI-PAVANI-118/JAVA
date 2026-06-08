d9398f85292fa33d018121abd622f5bd77597b8d
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-06-08 15:21:36 +0530

    Add files via upload

--- Factorial.java
+++ Factorial.java
@@ -1,15 +1,14 @@
-//Print factorial of a number.
 import java.util.Scanner;
-public class Hello{
-  public static void main(String[]args){
-  Scanner sc = new Scanner(System.in);
-  System.out.println("ENTER THE VALUE OF N:");
-  int n = sc.nextInt();
-  int fact=1;
-  for(int i=1;i<=n;i++){
-    fact=fact * i;
-  }
-   System.out.println("FACTORIAL OF ENTERE NUMBER IS " + fact);
-    sc.close();
-  }
+public class Factorial {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("ENTER THE FACTORIAL NUMBER:");
+        int n=sc.nextInt();
+        int factorial=1;
+        for(int i=1;i<=n;i++){
+            factorial *= i;
+        }
+        System.out.println(n+"! IS:"+factorial);
+        sc.close();
+    }
 }
