327cda8d40c15f20b4f31d7b123877a7a12c72bd
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-06-10 05:23:52 +0530

    Add files via upload

--- Square.java
+++ Square.java
@@ -1,9 +1,7 @@
-public class Square{
-  public static void main(String[]args){
-    System.out.println("* * * * *");
-    System.out.println("*       *");
-    System.out.println("*       *");
-    System.out.println("*       *");
-    System.out.println("* * * * *");
-  }
+public class Square {
+    public static void main(String[]args){
+        for(int i=1;i<=20;i++){
+            System.out.println("square of number "+i+" is "+(i*i));
+        }
+    }    
 }
