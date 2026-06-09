71272372e039ad01afafc655ef8387e0ccc757f2
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-06-10 05:23:11 +0530

    Add files via upload

--- Reverse.java
+++ Reverse.java
@@ -1,17 +1,16 @@
 import java.util.Scanner;
-public class Reverse{
-  public static void main(String[]args){
-  Scanner sc = new Scanner(System.in);
-  System.out.println("ENTER THE VALUE OF N:");
-  int n = sc.nextInt();
-  int reverse=0;
-  while (n > 0){
-    int digit = n%10;
-    reverse = reverse * 10 +digit;
-    n =n/10;
-  }
-    System.out.println("REVERSE OF THE NUM IS "+reverse);
-    sc.close();
-  }
-
+public class Reverse {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("ENTER THE NUMBER:");
+        int num=sc.nextInt();
+        int reverse=0;
+        while(num>0){
+            int digit=num%10;
+            reverse=reverse*10+digit;
+            num=num/10;
+        }
+        System.out.println("REVERSED NUMBER IS:"+reverse);
+        sc.close();
+    }
 }
