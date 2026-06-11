0593fbeab810fcfa9b9273d783a77f231eabfa80
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-06-11 17:56:16 +0530

    Add files via upload

--- Character.java
+++ Character.java
@@ -1,11 +1,11 @@
-import java.util.Scanner;
-
 public class Character {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("enter the character: ");
-        char ch = sc.next().charAt(0); /
-        System.out.println("character: " + ch);
-        sc.close();
+    public static void main(String[]args){
+        char ch='e';
+        for(char x='a';x<=ch;x++){
+            for(char y='a';y<=x;y++){
+                System.out.print(y+" ");
+            }
+            System.out.println();
+        }
     }
 }
