7e705a47c5f82f16efba5f9b576a91e542e7478d
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-06-13 12:08:18 +0530

    Add files via upload

---
+++ Star_Decreasing_Triangle.java
@@ -0,0 +1,12 @@
+public class Star_Decreasing_Triangle{
+    public static void main(String[]args){
+        int n=1;
+        for(int i=4;i>=n;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
