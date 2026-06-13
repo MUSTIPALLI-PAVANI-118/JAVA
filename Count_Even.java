1d76b0e527e7721c3793bd2f0cc152ec4b4b06c4
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-06-13 12:11:23 +0530

    Add files via upload

---
+++ Count_Even.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Count_Even{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("ENTER ARRAY SIZE:");
+        int n=sc.nextInt();
+        int []arr=new int[n];
+        int count=0;
+        System.out.println("ENTER ARRAY ELEMENTS:");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            if(arr[i]%2==0){
+                count+=1;
+            }
+        }
+        System.out.println("COUNT OF EVEN ARRAY ELEMENTS IS:"+count);
+        sc.close();
+    }
+}
