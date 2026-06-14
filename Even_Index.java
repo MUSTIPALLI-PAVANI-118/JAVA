791fa9511d72b58f408eca18768e5f9adb266fd6
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-06-14 12:12:23 +0530

    Add files via upload

---
+++ Even_Index.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Even_Index {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("ENTER THE SIZE OF ARRAY:");
+        int n=sc.nextInt();
+        int []arr=new int[n];
+        System.out.println("ENTER ARRAY ELEMENTS:");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("ARRAY INDEX ELEMENTS ARE:");
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                System.out.println(arr[i]+"");
+            }
+            
+        }
+        sc.close();
+        
+    }
+}
