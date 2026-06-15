2f87bafbdde25f907f1c7ba93c59f44d59418fc7
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-06-15 06:51:08 +0530

    Add files via upload

---
+++ Negative.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Negative{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("ENTER ARRAY SIZE:");
+        int n=sc.nextInt();
+        int []arr=new int[n];
+        int count=0;
+        System.out.println("ENTER ARRAY ELEMENTS:");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            if(arr[i]<0){
+                count+=arr[i];
+            }
+        }
+        System.out.println("SUM OF NEGATIVE ARRAY ELEMENTS IS:"+count);
+        sc.close();
+    }
+}
