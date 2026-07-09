e0710fd74ac5aee379bd1c8f63263ccf4f15b1d4
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-07-09 22:47:44 +0530

    Add files via upload

---
+++ Final.java
@@ -0,0 +1,6 @@
+public class Final {
+    public static void main(String[]args){
+        final String school_name="SRI CHAITANYA";
+        System.out.println("my final scholl name is "+school_name);
+    }
+}
