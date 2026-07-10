a32ef95024d8f777882369ee0451a23bc3cd6b77
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-07-10 22:39:47 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,12 @@
+public class Employee {
+    public static void main(String[]args){
+        int ID=25028;
+        double salary=50000.90;
+        int department_code=002;
+        boolean availability=true;
+        System.out.println("EMPLOYEE ID IS "+ID);
+        System.out.println("EMPLOYEE SALARY IS "+salary);
+        System.out.println("DEPARTMENT CODE IS "+department_code);
+        System.out.println("AVAILABILITY STATUS IS "+availability);
+    }
+}
