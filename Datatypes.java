5f95d0bb3d4db85ef7768d92e47ded96824e8cf4
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-07-10 22:40:22 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,14 @@
+public class Datatypes {
+    public static void main(String[]args){
+        String name="pavani";
+        int age=19;
+        char gender='F';
+        double height=5.3;
+        boolean isstudent=true;
+        System.out.println("NAME IS "+name);
+        System.out.println("AGE IS "+age);
+        System.out.println("GENDER IS "+gender);
+        System.out.println("HEIGHT IS "+height);
+        System.out.println("STUDENT IS "+isstudent);
+    }
+}
