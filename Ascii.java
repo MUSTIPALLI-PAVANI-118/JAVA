62d06f3518d8d7affbb2151cb602357427270750
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-07-12 19:47:02 +0530

    Add files via upload

--- Ascii.java
+++ Ascii.java
@@ -2,10 +2,13 @@ import java.util.Scanner;
 public class Ascii {
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
-        System.out.println("ENTER THE CHARCHACTER:");
-        char ch=sc.next().charAt(0);
-        int ascii=(int)ch;
-        System.out.println("ASCII VALUE OF CHARACTER IS:"+ascii);
+        System.out.println("enter 1st character:");
+        char ch1=sc.next().charAt(0);
+        System.out.println("enter 2nd character:");
+        char ch2=sc.next().charAt(0);
+        System.out.println(ch1>ch2);
+        System.out.println(ch1<ch2);
+        System.out.println(ch1==ch2);
         sc.close();
     }
 }
