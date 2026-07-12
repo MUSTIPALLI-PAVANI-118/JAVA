e30cbb099d817681958ecdc315d02119bbaa0a06
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-07-12 19:48:02 +0530

    Add files via upload

--- Assign_Value.java
+++ Assign_Value.java
@@ -2,6 +2,6 @@ public class Assign_Value {
     public static void main(String[]args){
         int value;
         value=10;
-        System.out.println("VALUE IS "+value);
+        System.out.println("VALUE IS "+value);                         
     }
 }
