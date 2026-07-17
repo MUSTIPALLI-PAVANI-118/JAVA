6288b6c21f248fb2c756a51a06e37a89681c4c81
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-07-17 08:01:14 +0530

    Add files via upload

--- Digit.java
+++ Digit.java
@@ -1,15 +1,7 @@
-import java.util.Scanner;
 public class Digit {
     public static void main(String[]args){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("enter character:");
-        char ch=sc.next().charAt(0);
-        if(ch>='0' && ch<='9'){
-            System.out.println("CHARACTER IS DIGIT");
-        }
-        else{
-            System.out.println("CHARACTER IS NOT A DIGIT");
-        }
-        sc.close();
+        char ch=0;
+        System.out.println("number is "+(ch<=0 || ch>=9));
+
     }
 }
