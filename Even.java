db0ba1ac64d8668c6ff441e571290491038c3950
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-07-17 08:02:18 +0530

    Add files via upload

--- Even.java
+++ Even.java
@@ -2,22 +2,13 @@ import java.util.Scanner;
 public class Even {
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
-        System.out.println("ENTER THE SIZE OF ARRAY:");
-        int n=sc.nextInt();
-        int []arr=new int[n];
-        int sum=0;
-        System.out.println("ENTER ARRAY ELEMENTS:");
-        for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
+        System.out.println("ENTER A NUMBER:");
+        int num=sc.nextInt();
+        if(num%2==0){
+            System.out.println("NUMBER IS EVEN");
+        }else{
+            System.out.println("NUMBER IS ODD");
         }
-        for(int i=0;i<n;i++){
-            if(arr[i]%2==0){
-                sum=sum+arr[i];
-            }
-        }
-        
-        System.out.println("SUM OF EVEN ARRAY ELEMENTS IS:"+sum);
         sc.close();
-        
     }
-} 
+}
