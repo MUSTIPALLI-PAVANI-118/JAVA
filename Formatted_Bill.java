4729cad3407383c7a6c27ac8c911810653b15954
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-07-18 08:52:27 +0530

    Add files via upload

---
+++ Formatted_Bill.java
@@ -0,0 +1,10 @@
+public class Formatted_Bill {
+    public static void main(String[]args){
+        String name="pavani"; 
+        double price=92.90;
+        int quantity=10;
+        System.out.printf("PRODUCT NAME IS %s%n",name);
+        System.out.printf("PRODUCT PRICE IS %f%n",price);
+        System.out.printf("QUANTITY IS %d%n",quantity);
+    }
+}
