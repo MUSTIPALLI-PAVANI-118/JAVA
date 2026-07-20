03c85e1cf70e1763d68fdb0461c2ac40b65a3931
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-07-20 16:28:56 +0530

    Add files via upload

---
+++ Greater.java
@@ -0,0 +1,10 @@
+public class Greater {
+    public static void main(String[]args){
+        int a=6;
+        int b=9;
+        int max=(a>b)? a:b;
+        int min=(a<b)? a:b;
+        System.out.println("MAXIMUM VALUE "+max);
+        System.out.println("MINIMUM VALUE "+min);
+    }
+}
