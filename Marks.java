a202f280a8904a33fd01ada9018dc5f8e98ff9e5
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-07-25 06:29:31 +0530

    Add files via upload

--- Marks.java
+++ Marks.java
@@ -1,23 +1,10 @@
-import java.util.Scanner;
 public class Marks {
     public static void main(String[]args){
-        Scanner sc=new Scanner(System.in);
-        System.out.print("enter subject 1 marks:");
-        int a=sc.nextInt();
-        System.out.print("enter subject 2 marks:");
-        int b=sc.nextInt();
-        System.out.print("enter subject 3 marks:");
-        int c=sc.nextInt();
-        System.out.print("enter subject 4 marks:");
-        int d=sc.nextInt();
-        System.out.print("enter subject 5 marks:");
-        int e=sc.nextInt();
-        int total=a+b+c+d+e;
-        double average=(total)/5.0;
-        System.out.println("average of numbers:"+average);
-        double percentage=(total/500.0)*100;
-        System.out.println("percentage of numbers:"+percentage);
-        sc.close();
+        int num1=98;
+        int num2=99;
+        int num3=78;
+        double marks=(num1+num2+num3)/300.0;
+        double percentage=marks*100;
+        System.out.printf("PERECENTAGE IS %f",percentage);
     }
-    
 }
