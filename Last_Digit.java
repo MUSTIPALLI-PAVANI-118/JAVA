5bf49809dc98a58ed41b033b204919b7d8f315ae
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-07-25 06:27:11 +0530

    Add files via upload

--- Last_Digit.java
+++ Last_Digit.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 public class Last_Digit {
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
-        System.out.println("ENTER THE NUMBER:");
+        System.out.print("ENTER THE NUMBER:");
         int num=sc.nextInt();
-        int lastdigit=num%100;
-        System.out.println("last digit of number is:"+lastdigit);
+        int last_digit=num%10;
+        int last_two_digits=num%100;
+        System.out.println("LAST DIGIT IS:"+last_digit);
+        System.out.println("LAST TWO DIGIT IS:"+last_two_digits);
         sc.close();
     }
 }
