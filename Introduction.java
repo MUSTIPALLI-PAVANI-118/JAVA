fb63bc8c05f59166648adc6afab936f0c3e11a3d
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-07-25 06:26:44 +0530

    Add files via upload

---
+++ Introduction.java
@@ -0,0 +1,8 @@
+public class Introduction {
+    public static void main(String[]args){
+        System.out.println("HI,GOOD MORNING EVERYONE");
+        System.out.println("MY NAME IS PAVANI");
+        System.out.println("IAM STUDYING ARTIDFICAL INTELLIGENCE AND DATA SCIENCE");
+        System.out.println("THANKS FOR GIVING A CHANCE TO INTRODUCE MYSELF");
+    }
+}
