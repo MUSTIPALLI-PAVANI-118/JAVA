fd75f3459c2604374eca77ed1f0e051198a28b17
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-07-25 06:28:49 +0530

    Add files via upload

---
+++ Long_Value.java
@@ -0,0 +1,6 @@
+public class Long_Value {
+    public static void main(String[]args){
+        long population=1000000000000000000L;
+        System.out.println("MY COUNTRY POPULATION IS "+population);     
+    }
+}
