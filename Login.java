46a5e83010b98be96fbd99d1d8b9f4f2644b2dbf
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-07-25 06:28:04 +0530

    Add files via upload

---
+++ Login.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Login {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter boolean login id");
+        
+        sc.close();
+    }
+}
