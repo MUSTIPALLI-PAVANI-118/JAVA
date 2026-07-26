bee3f4927f9293ae2daad51cea7c62af1d3778cd
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-07-26 09:12:57 +0530

    Add files via upload

---
+++ Number2.java
@@ -0,0 +1,6 @@
+public class Number2 {
+    public static void main(String[]args){
+        int num=899;
+        System.out.println("check whether number is less than 0 greater than 100 "+(num<0 || num>100));
+    }
+}
