56c5bc5053cf754ddabbaa7f1c26113ffd8bff84
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-07-26 09:10:35 +0530

    Add files via upload

--- Name.java
+++ Name.java
@@ -2,10 +2,9 @@ import java.util.Scanner;
 public class Name{
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter your name:");
+        System.out.println("ENTER YOUR NAME:");
         String name=sc.nextLine();
-        System.out.println("wish you a good morning "+name);
+        System.out.println("my name is: "+name);
         sc.close();
     }
 }
-
