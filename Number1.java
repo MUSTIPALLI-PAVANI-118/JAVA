0137a57421828f55e5e9795179c03e3694bb2e72
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-07-26 09:12:30 +0530

    Add files via upload

---
+++ Number1.java
@@ -0,0 +1,6 @@
+public class Number1 {
+    public static void main(String[]args){
+        int num=89;
+        System.out.println("check whether number is greater than 10 less than 100 "+(num>10 && num<100));
+    }
+}
