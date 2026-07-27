950213a03fb62ccd853d67a4d22c98d9f1efc03d
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-07-27 07:08:02 +0530

    Add files via upload

---
+++ Paragraph.java
@@ -0,0 +1,7 @@
+public class Paragraph {
+    public static void main(String[]args){
+        System.out.println("JAVA IS A PROGRAMMING LANGUAGE.");
+        System.out.println("IT IS AN OBJECT ORIENTED ORIENTED LANGUAGE.");
+        System.out.println("IT IS A PLATFORM INDEPENDENT LANGUAGE.");
+    }
+}
