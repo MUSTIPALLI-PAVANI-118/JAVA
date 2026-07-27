dd9b621046bf46bb5b560ceb2fa982afaab52224
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-07-27 07:06:21 +0530

    Add files via upload

---
+++ Old_New.java
@@ -0,0 +1,8 @@
+public class Old_New {
+    public static void main(String[]args){
+        int old=10;
+        System.out.println("OLD VALUE IS "+old);
+        old=20;
+        System.out.println("NEW VALUE IS "+old);
+    }
+}
