37a6e162729bb3e4b6e1c3e594df3c40f2feb02c
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-07-27 07:05:57 +0530

    Add files via upload

---
+++ Numbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Numbers {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter number:");
+        int n=sc.nextInt();
+        System.out.println("enter number:");
+        int m=sc.nextInt();
+        n *=m;
+        System.out.println("UPDATED VALUE :"+n);
+        n /=m;
+        System.out.println("UPDATED VALUE:"+n);
+        n%=m;
+        System.out.println("UPDATED VALUE:"+n);
+        sc.close();
+    }
+}
