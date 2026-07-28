de9b9e80468a45a7f13b0905b88f66e57b89dddd
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-07-28 11:38:32 +0530

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,12 @@
+public class Program {
+    public static void main(String[]args){
+        String name="pavani";
+        int age=19;
+        char grade='A';
+        double percentage=90;
+        System.out.println("MT NAME IS "+name);
+        System.out.println("MY AGE IS "+age);
+        System.out.println("MY GRADE IS "+grade);
+        System.out.println("MY PERCENTAGE IS "+percentage);
+    }
+}
