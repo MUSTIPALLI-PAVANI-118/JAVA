65ca9c5b61edcaa9d81271c1e037b64169e7b4c9
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-07-28 11:35:10 +0530

    Add files via upload

---
+++ Print.java
@@ -0,0 +1,10 @@
+public class Print {
+    public static void main(String[]args){
+        String name="pavani";
+        int age=18;
+        String city="kurnool";
+        System.out.println("MY NAME IS:"+name);
+        System.out.println("MY AGE IS:"+age);
+        System.out.println("I LIVE IN "+city);
+    }
+}
