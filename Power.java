30359f55291513a0c599fca4a38b825cc576dd06
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-07-28 11:34:46 +0530

    Add files via upload

--- Power.java
+++ Power.java
@@ -2,15 +2,13 @@ import java.util.Scanner;
 public class Power {
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
-        System.out.println("ENTER THE NUMBER:");
+        System.out.println("enter the number:");
         int n=sc.nextInt();
-        System.out.println("ENTER THE POWER:");
+        System.out.println("enter power of n:");
         int p=sc.nextInt();
-        int result =1;
-        for(int i=1;i<=p;i++){
-            result *=n;
+        for(int i=1;i<=n;i++){
+            
         }
-        System.out.println("POWER OF NUMBER IS:"+result);
         sc.close();
     }
 }
