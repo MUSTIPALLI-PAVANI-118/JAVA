f74fbd637345533521babbffaac8b01f6644c8a7
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-07-28 11:36:17 +0530

    Add files via upload

---
+++ Product_Name.java
@@ -0,0 +1,12 @@
+public class Product_Name {
+    public static void main(String[]args){
+        String product_name="chocolate";
+        int quantity=10;
+        double price=10.00;
+        boolean availaibility=true;
+        System.out.println("PRODUCT NAME IS "+product_name);
+        System.out.println("PRICE OF PRODUCT "+price);
+        System.out.println("QUANTITY OF PRODUCT IS "+quantity);
+        System.out.println("AVAILABILITY STATUS OF PRODUCT "+availaibility);
+    }
+}
