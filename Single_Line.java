e99402892475858f7b7dcff917749d0621dafab6
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-07-30 20:48:25 +0530

    Add files via upload

---
+++ Single_Line.java
@@ -0,0 +1,8 @@
+public class Single_Line {
+    public static void main(String[]args){
+        String name="pavani";
+        int age=19;
+        double marks=98.00;
+        System.out.println("MY NAME IS:"+ name +" MY AGE IS: "+age +" I GOT "+marks +" marks in maths");
+    }
+}
