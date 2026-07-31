53553fe0d31a722cf69ed25033f887d90fc6faec
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-07-31 06:48:22 +0530

    Add files via upload

---
+++ Student_Marks.java
@@ -0,0 +1,8 @@
+public class Student_Marks {
+    public static void main(String[]args){
+        int marks1=100;
+        int marks2=90;
+        marks1+=marks2;
+        System.out.println("TOTAL MARKS IS:"+marks1);
+    }    
+}
