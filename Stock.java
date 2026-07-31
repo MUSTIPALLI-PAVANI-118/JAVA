43db4c2421461ef094d9a2001c96f23f04ccbcfb
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-07-31 06:45:06 +0530

    Add files via upload

---
+++ Stock.java
@@ -0,0 +1,8 @@
+public class Stock {
+    public static void main(String[]args){
+        int quantity=10;
+        int reduced=3;
+        quantity-=reduced;
+        System.out.println("REMAINING STOCK IS:"+quantity);
+    }
+}
