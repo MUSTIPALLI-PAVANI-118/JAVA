53d5686a98cece13c465bf8f1ba131a7b4783fb9
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-07-31 06:43:36 +0530

    Add files via upload

--- Square.java
+++ Square.java
@@ -1,11 +1,13 @@
+import java.util.Scanner;
 public class Square {
     public static void main(String[]args){
-        int n=6;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
+        Scanner sc=new Scanner(System.in);
+        System.out.print("ENTER A NUMBER:");
+        int num=sc.nextInt();
+        int square=num*num;
+        int cube=num*num*num;
+        System.out.println("SQUARE OF NUMBER IS:"+square);
+        System.out.println("CUBE OF NUMBER IS:"+cube);
+        sc.close();
     }
 }
