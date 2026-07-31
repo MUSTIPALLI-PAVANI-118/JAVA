a060af075604080cac97a2929bb513d1707f4b6b
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-07-31 06:48:57 +0530

    Add files via upload

---
+++ Student_Name.java
@@ -0,0 +1,16 @@
+public class Student_Name {
+    public static void main(String[]args){
+        String name="PAVANI";
+        System.out.println("STUDENT NAME IS :"+name);
+        int age=19;
+        System.out.println("STUDENRT AGE IS "+age);
+        String roll_no="CH.AI.U4AID25028";
+        System.out.println("STUDENT ROLL NUMBER IS "+roll_no);
+        int marks=90;
+        System.out.println("STUDENT MARKS IS "+marks);
+        char grade='A';
+        System.out.println("STUDENT GRADE IS "+grade);
+        boolean result=true;
+        System.out.println("STUDENT RESULT STATUS IS "+result);
+    }
+}
