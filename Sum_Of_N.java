944fd001ab9a5de684d614ba2b80cb6f5c0c7459
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-08-01 06:07:14 +0530

    Add files via upload

---
+++ Sum_Of_N.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Sum_Of_N {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter 'n'");
+        int n=sc.nextInt();
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            sum+=i;
+        }
+        System.out.println("sum of natural numbers:"+sum);
+        sc.close();
+    }
+}
+    
