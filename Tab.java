2b1121544ead754e48899ca34149564295959e69
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-08-01 06:08:39 +0530

    Add files via upload

---
+++ Tab.java
@@ -0,0 +1,10 @@
+public class Tab {
+    public static void main(String[]args){
+        String name="PAVANI";
+        String roll_no="CH.AI.U4AID25028";
+        String department="AI & DS";
+        System.out.println("MY NAME IS:"+name);
+        System.out.println("MY ROLL NO IS:"+roll_no);
+        System.out.println("MY DEPARTMENT IS:"+ department);
+    }
+}
