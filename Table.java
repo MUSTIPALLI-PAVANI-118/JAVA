01ab027057b56eed2650074ad9d4f400fab2ee4e
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-08-01 06:09:03 +0530

    Add files via upload

---
+++ Table.java
@@ -0,0 +1,11 @@
+public class Table {
+    public static void main(String[]args){
+        String name="pavani";
+        int roll_no=28;
+        int marks1=98;
+        int marks=99;
+        System.out.printf("%-10s %-10s %-10s %-10s%n", "Name", "Roll No", "Marks1", "Marks");
+        System.out.printf("%-10s %-10d %-10d %-10d%n", name, roll_no, marks1, marks);
+
+    }
+}
