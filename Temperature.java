9bab2bd3a5670db0662225228a18b4333272a2d8
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-08-01 06:10:12 +0530

    Add files via upload

---
+++ Temperature.java
@@ -0,0 +1,7 @@
+public class Temperature {
+    public static void main(String[]args){
+        double celsius=99.9;
+        double f=((celsius*9)/5)+32;
+        System.out.println("faranheit temperature:"+f);
+    }   
+}
