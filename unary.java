8677a38deb5328e7af973068ae1d6000b8e8c59c
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-08-04 07:50:37 +0530

    Add files via upload

---
+++ unary.java
@@ -0,0 +1,14 @@
+public class unary {
+    public static void main(String[]args){
+        int num=1;
+        System.out.println("before unary plus"+num);
+        System.out.println("after unary plus"+(+num));
+        int m=9;
+        System.out.println("before unary minus"+m);
+        System.out.println("after unary minus"+(-m));
+        double temperature=99.99;
+        System.out.println("temperature after unary minus"+(-temperature));
+        int n=-9;
+        System.out.println("positive number "+(-n));
+    }
+}
