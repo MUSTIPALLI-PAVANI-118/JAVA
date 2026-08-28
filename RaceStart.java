201e6d1db0f194d4dc922026de680687df2e04f3
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-08-28 19:58:56 +0530

    Create RaceStart.java

---
+++ RaceStart.java
@@ -0,0 +1,19 @@
+public class RaceStart {
+
+    public static void main(String[] args) {
+
+        String driver = "Lewis Hamilton";
+        int startingPosition = 3;
+
+        System.out.println("===== F1 RACE START =====");
+        System.out.println("Driver: " + driver);
+        System.out.println("Starting Position: P" + startingPosition);
+
+        System.out.println("\n5...");
+        System.out.println("4...");
+        System.out.println("3...");
+        System.out.println("2...");
+        System.out.println("1...");
+        System.out.println("🏁 LIGHTS OUT! RACE STARTED!");
+    }
+}
