915f5d44a03dc6cf8b3546a0df9735bcfae79c3b
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-08-30 22:27:27 +0530

    Add files via upload

---
+++ Divisibile_5.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Divisibile_5 {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("ENTER THE NUMBER:");
+        int n=sc.nextInt();
+        if(n%5==0){
+            System.out.println("DIVISIBLE BY 5");
+        }else{
+            System.out.println("NOT DIVISIBLE BY 5");
+        }
+        sc.close();
+    }
+}
