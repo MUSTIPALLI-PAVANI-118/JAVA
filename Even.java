4c519131a3c3552cf70a433c069b54ce4fec330b
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-08-31 18:38:01 +0530

    Add files via upload

--- Even.java
+++ Even.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 public class Even {
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
-        System.out.println("ENTER A NUMBER:");
-        int num=sc.nextInt();
-        if(num%2==0){
-            System.out.println("NUMBER IS EVEN");
+        System.out.println("ENTER THE NUMBER:");
+        int n=sc.nextInt();
+        if(n%2==0){
+            System.out.println("number is even");
         }else{
-            System.out.println("NUMBER IS ODD");
+            System.out.println("number is not even");
         }
         sc.close();
     }
