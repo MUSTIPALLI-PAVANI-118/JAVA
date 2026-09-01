2c1d77a865704437a7028010e62e2f5c8e1eb5a1
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-09-01 09:40:24 +0530

    Add files via upload

--- Profit_Loss.java
+++ Profit_Loss.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 public class Profit_Loss {
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter the cost price:");
+        System.out.println("enter cost price:");
         double cp=sc.nextDouble();
-        System.out.println("enter the selling price:");
+        System.out.println("enter selling price:");
         double sp=sc.nextDouble();
-        if(sp>cp){
+        double amount=sp-cp;
+        if(amount>0){
             System.out.println("profit");
-        }else if(sp<cp){
+        }
+        else{
             System.out.println("loss");
-        }else{
-            System.out.println("no profit or no loss");
         }
         sc.close();
     }
