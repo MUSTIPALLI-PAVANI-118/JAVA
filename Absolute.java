a991dced3d0827cf111310b10cb5880bdd8d4980
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-09-01 09:38:54 +0530

    Add files via upload

--- Absolute.java
+++ Absolute.java
@@ -2,15 +2,14 @@ import java.util.Scanner;
 public class Absolute {
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter the number");
-        int num=sc.nextInt();
-        if(num>=0){
-            System.out.println("absolute value is:"+num);
+        System.out.println("enter the number:");
+        int n=sc.nextInt();
+        if(n>=0){
+            System.out.println("absolute value is:"+n);
         }
         else{
-            System.out.println("absolute value is:"+(-num));
+            System.out.println("absolute value is:"+(-n));
         }
         sc.close();
     }
-    
 }
