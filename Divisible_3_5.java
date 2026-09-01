2ed1fd8d335db66c491e7e0f4c52372ac1406981
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-09-01 09:39:59 +0530

    Add files via upload

--- Divisible_3_5.java
+++ Divisible_3_5.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
-public class Divisible_3_5 {
+public class Divisible_3_5{
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter a number:");
-        int num=sc.nextInt();
-        if(num%3==0 && num%5==0){
-            System.out.println("number is divisible by both 3 and 5");
-        }else{
-            System.out.println("number is not divisible by both 3 and 5");
+        System.out.println("enter number:");
+        int n=sc.nextInt();
+        if(n%5==0&&n%3==0){
+            System.out.println("number is divible by both 3 and 5");
         }
-        sc.close();
+        else{
+            System.out.println("number is not divible by both 3 and 5");
+        }
+        sc.close(); 
     }
 }
