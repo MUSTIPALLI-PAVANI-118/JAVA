08f504cead02ad728c4647b7bab735a5646b8ffd
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-09-02 22:55:46 +0530

    Add files via upload

--- Percentage.java
+++ Percentage.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
 public class Percentage {
     public static void main(String[]args){
-        double percentage=98;
-        System.out.println("MY PERCENTAGE IS:"+percentage);
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter percentage:");
+        double p=sc.nextDouble();
+        if(p>=90){
+            System.out.println("distinction");
+        }
+        else if(p>=75){
+            System.out.println("first class");
+        }
+        else if(p>=50){
+            System.out.println("second class");
+        }
+        else if(p>=35){
+            System.out.println("pass");
+        }
+        else{
+            System.out.println("fail");
+        }
+        sc.close();
     }
 }
