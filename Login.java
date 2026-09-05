bf97032b4404f0447ce9d4bc9a1533c3c9b4e504
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-09-05 21:57:19 +0530

    Add files via upload

--- Login.java
+++ Login.java
@@ -2,8 +2,16 @@ import java.util.Scanner;
 public class Login {
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter boolean login id");
-        
+        System.out.println("enter the boolean username:");
+        boolean username=sc.nextBoolean();
+        System.out.println("enter the boolean password:");
+        boolean password=sc.nextBoolean();
+        if(username==true && password==true){
+            System.out.println("LOGIN SUCCESSFUL");
+        }
+        else{
+            System.out.println("invalid crediantials");
+        }
         sc.close();
     }
 }
