97626bd3e6cdbe87721b9bfb0c17c06408663029
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-09-06 09:51:13 +0530

    Add files via upload

---
+++ Switch_Character.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class Switch_Character {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter character:");
+        char ch=sc.next().charAt(0);
+        switch(ch){
+            case 'a':
+                System.out.println("vowel");
+                break;
+            case 'e':
+                System.out.println("vowel");
+                break;
+            case 'i':
+                System.out.println("vowel");
+                break;
+            case 'o':
+                System.out.println("vowel");
+                break;
+            case 'u':
+                System.out.println("vowel");
+                break;
+            case 'A':
+                System.out.println("vowel");
+                break;
+            case 'E':
+                System.out.println("vowel");
+                break;
+            case 'I':
+                System.out.println("vowel");
+                break;
+            case 'O':
+                System.out.println("vowel");
+                break;
+            case 'U':
+                System.out.println("vowel");
+                break;
+            default:
+                System.out.println("consonants");
+            
+        }
+        sc.close();
+    }
+}
