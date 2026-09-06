682b3b3adb16a480d5ab8c724f819bf6a11055f0
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-09-06 09:49:47 +0530

    Add files via upload

---
+++ Weekday.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Weekday {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter choice:");
+        int choice=sc.nextInt();
+        switch(choice){
+            case 1:
+                System.out.println("sunday");
+                break;
+            case 2:
+                System.out.println("monday");
+                break;
+            case 3:
+                System.out.println("tuesday");
+                break;
+            case 4:
+                System.out.println("wednesday");
+                break;
+            case 5:
+                System.out.println("thursday");
+                break;
+            case 6:
+                System.out.println("friday");
+                break;
+            case 7:
+                System.out.println("saturday");
+                break;
+            default:
+                System.out.println("invalid choice");
+        }
+
+        sc.close();
+    }
+}
