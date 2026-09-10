019128ad5bce1940f0289e1245ec85c1b6405b1d
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-09-10 19:59:35 +0530

    Add files via upload

---
+++ Course_Code.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Course_Code {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        
+        sc.close();
+    }
+}
