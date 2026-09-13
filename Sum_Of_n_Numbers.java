83bda6c797f989174926f56d1fe2ccab8b956ab3
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-09-13 08:08:17 +0530

    Add files via upload

---
+++ Sum_Of_n_Numbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Sum_Of_n_Numbers {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number:");
+        int n=sc.nextInt();
+        int sum =0;
+        for(int i=1;i<=n;i++){
+            sum +=i;
+        }
+        System.out.println("sum of numbers is "+sum);
+        sc.close();
+    }
+}
