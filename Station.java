d218c3090a871f98e674bc9607ba4bec13427f79
MUSTIPALLI PAVANI <mustipallipavani118@gmail.com>
2026-09-23 07:36:41 +0530

    Create Station.java

---
+++ Station.java
@@ -0,0 +1,17 @@
+public class Station {
+    private final String name;
+    private final String location;
+
+    public Station(String name, String location) {
+        this.name = name;
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+}
